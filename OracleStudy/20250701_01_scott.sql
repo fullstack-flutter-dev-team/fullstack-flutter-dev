0c960d0e2d16d7456691995f8fa39bfc1b3c439d
quantum kim <quantumearth76@gmail.com>
2025-07-01 17:57:42 +0900

    7월 1일 (화) 오후 수업

--- OracleStudy/20250701_01_scott.sql
+++ OracleStudy/20250701_01_scott.sql
@@ -429,7 +429,7 @@ FROM DUAL;
       , TRUNC(48.674, -2) "COL8" -- 0
       , TRUNC(48.674, -3) "COL9" -- 0
 FROM DUAL;
---==>> 48.768	48.67	48.67	48.6	48	48	40	0	0
+--==>> 48.768    48.67    48.67    48.6    48    48    40    0    0
 
 
 --▣ MOD() 나머지를 반환하는 함수 → 『%』 
@@ -457,7 +457,7 @@ FROM DUAL;
 SELECT LOG(10, 100) "COL1"
      , LOG(10, 20)  "COL2"
 FROM DUAL;
---==>> 2	        1.30102999566398119521373889472449302677
+--==>> 2            1.30102999566398119521373889472449302677
 
 
 --▣ 삼각함수
@@ -465,8 +465,8 @@ SELECT SIN(1), COS(1), TAN(1)
 FROM DUAL;
 --==>> SIN, COS, TAN 결과값을 반환한다.
 /*
-0.8414709848078965066525023216302989996233	
-0.5403023058681397174009366074429766037354	
+0.8414709848078965066525023216302989996233    
+0.5403023058681397174009366074429766037354    
 1.55740772465490223050697480745836017308
 */
 
@@ -475,8 +475,8 @@ SELECT ASIN(0.5), ACOS(0.5), ATAN(0.5)
 FROM DUAL;
 --==>>
 /*
-0.52359877559829887307710723054658381405	
-1.04719755119659774615421446109316762805	
+0.52359877559829887307710723054658381405    
+1.04719755119659774615421446109316762805    
 0.4636476090008061162142562314612144020295
 */
 
@@ -487,7 +487,7 @@ SELECT SIGN(5-2) "COL1"
      , SIGN(5-5) "COL2"
      , SIGN(5-7) "COL3"
 FROM DUAL;
---==>> 1	0	-1
+--==>> 1    0    -1
 
 --▣ ASCII(), CHR() → 서로 대응(상응)하는 개념의 함수
 -- ASCII() - 문자를 건네주면 그 문자의 아스키 코드 값을 전달해 주는 함수
@@ -497,7 +497,7 @@ FROM DUAL;
 SELECT ASCII('A') "COL1"
      , CHR(65) "COL2"
 FROM DUAL;
---==>> 65	A
+--==>> 65    A
 
 
 --------------------------------------------------------------------------------------------------
@@ -527,7 +527,7 @@ FROM DUAL;
 --▣ 시간 단위 연산
 SELECT SYSDATE "COL1"          -- 2025-07-01 12:14:30 → 현재
      , SYSDATE + 1/24 "COL2"   -- 2025-07-01 13:14:30 → 1시간 후
-     , SYSDATE - 2/24 "COL3"   -- 2025-07-01 10:14:30 → 2시간 후
+     , SYSDATE - 2/24 "COL3"   -- 2025-07-01 10:14:30 → 2시간 전
 FROM DUAL;
 
 
@@ -548,7 +548,7 @@ FROM DUAL;
 /*
       현재시간                      연산 후 시간
 --------------------     -----------------------------
-2025-07-01 12:18:25	           2025-07-02 14:21:29
+2025-07-01 12:18:25               2025-07-02 14:21:29
 */
 
 -- [방법 - 2] ▶  모두 초로 환산해서 처리
@@ -625,7 +625,7 @@ SELECT NEXT_DAY(SYSDATE, '토') "COL1"
      , NEXT_DAY(SYSDATE, '월') "COL3"
 FROM DUAL;
 /*
-2025-07-05	2025-07-02	2025-07-07
+2025-07-05    2025-07-02    2025-07-07
 */
 
 --※ 날짜 관련 세션 설정 변경
@@ -649,7 +649,7 @@ SELECT NEXT_DAY(SYSDATE, 'SAT') "COL1"
      , NEXT_DAY(SYSDATE, 'WED') "COL2"
      , NEXT_DAY(SYSDATE, 'MON') "COL3"
 FROM DUAL;
---==>> 2025-07-05	2025-07-02	2025-07-07
+--==>> 2025-07-05    2025-07-02    2025-07-07
 
 
 --※ 날짜 관련 세션 설정 변경
@@ -662,15 +662,15 @@ SELECT NEXT_DAY(SYSDATE, '토') "COL1"
      , NEXT_DAY(SYSDATE, '수') "COL2"
      , NEXT_DAY(SYSDATE, '월') "COL3"
 FROM DUAL;
---==>> 2025-07-05	2025-07-02	2025-07-07
+--==>> 2025-07-05    2025-07-02    2025-07-07
 
 --▣ LAST_DAY()
 --> 해당 날짜가 포함되어 있는 그 달의 마지막 날을 반환한다.
-SELECT SYSDATE "COL1"                                      -- 2025-07-01	
+SELECT SYSDATE "COL1"                                      -- 2025-07-01    
      , LAST_DAY(SYSDATE) "결과확인"                         -- 2025-07-31
 FROM DUAL;
 
-SELECT SYSDATE "COL1"                                       -- 2025-07-01	
+SELECT SYSDATE "COL1"                                       -- 2025-07-01    
      , LAST_DAY(SYSDATE) "COL2"                             -- 2025-07-31
      , LAST_DAY(TO_DATE('2023-02-12', 'YYYY-MM-DD')) "COL3" -- 2023-02-28
      , LAST_DAY(TO_DATE('2020-02-12', 'YYYY-MM-DD')) "COL4" -- 2020-02-29
@@ -686,16 +686,386 @@ SELECT SYSDATE "COL1"
      , ADD_MONTHS(SYSDATE, 1) "COL2"
      , ADD_MONTHS(SYSDATE, 22) "전역 일자"
 FROM DUAL;
+/*
+2025-07-01    2025-08-01    2027-05-01
+*/
+
 --   2. 하루 꼬박꼬박 3끼 식사를 해야 한다고 가정하면,
 --      김한국이 몇 끼를 먹어야 집에 보내줄까??....
+--      복무기간 * 3
+--      --------
+--       (전역일자 - 현재일자)
+--       (전역일자 - 현재일자) * 3
+--SELECT (전역일자 - 현재일자) * 3
+--FROM DUAL;
+
+SELECT (ADD_MONTHS(SYSDATE, 22) - SYSDATE) * 3
+FROM DUAL;
+--==>> 2007
+
 SELECT SYSDATE "COL1"
      , ADD_MONTHS(SYSDATE, 1) "COL2"
      , ADD_MONTHS(SYSDATE, 22) "전역 일자"
+     , TO_NUMBER(TO_DATE(ADD_MONTHS(SYSDATE, 1)) - TO_DATE(SYSDATE, 'YYYY-MM-DD')) "식사 수"
      , TO_DATE(ADD_MONTHS(SYSDATE, 22)) - TO_DATE(SYSDATE, 'YYYY-MM-DD') "일수"
      , ADD_MONTHS(SYSDATE, 22)  - SYSDATE "일수"
      , TO_NUMBER(TO_DATE(ADD_MONTHS(SYSDATE, 22)) - TO_DATE(SYSDATE, 'YYYY-MM-DD'))*3 "식사 수"
 FROM DUAL;
+/*
+                                                            총 식사 수
+                                                             ------
+2025-07-01    2025-08-01    2027-05-01    31    669    669    2007
+*/
 
+
+--▣ 현재 날짜 및 시간을 기준으로...
+--   수료일(2025-11-05 18:00:00) 까지 남은 기간을..
+--   다음과 같은 형태로 조회할 수 있도록 쿼리문을 구성한다.
+/*
+-----------------------------------------------------------------------------
+현재시각            |  수료일              | 일   | 시간 | 분 | 초
+-----------------------------------------------------------------------------
+2025-07-01 14:31:27 | 2025-11-05 18:00:00 | 126 | 3    | 28 | 33
+-----------------------------------------------------------------------------
+*/
+
+-- 날짜 및 시간 관련 세션 설정 변경
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS';
+--==>> Session이(가) 변경되었습니다.
+
+--▣ 『1일 2시간 3분 4초』 를 ... 『초』 로 환산하면...
+SELECT  (1*24*60*60 + 60*60*2 + 60*3 + 4) "초"
+FROM DUAL;
+--==>> 93784
+
+
+-- 『93784초』를 ... 다시.『일, 시간, 분, 초』로 환산하면...
+SELECT TRUNC(TRUNC(TRUNC(93784/60)/60)/24) "일"
+     , MOD(TRUNC(TRUNC(93784/60)/60),24)"시간"
+     , TRUNC(93784/60) - TRUNC(TRUNC(93784/60)/60)*60 "분"
+     , 93784 - TRUNC(93784/60)*60 "초"
+FROM DUAL;
+--==>> 1    2    3    4
+
+-- 『93784초』를 ... 다시.『일, 시간, 분, 초』로 환산하면...
+SELECT TRUNC(TRUNC(TRUNC(93784/60)/60)/24) "일"
+     , MOD(TRUNC(TRUNC(93784/60)/60), 24)"시간"
+     , MOD(TRUNC(93784/60), 60) "분"
+     , MOD(93784, 60) "초"
+FROM DUAL;
+
+-- 『초』로 환산한 결과값은 93784초 이다.
+
+-- -- 수료일까지 남은 기간 확인(날짜 기준) --> 단위 : 일수
+-- SELECT 수료일자 - 현재일자
+-- FROM DUAL;
+
+-- 수료일자
+SELECT TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') "수료일"   
+FORM DUAL;
+
+-- 일수
+SELECT TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - STSDATE  
+FORM DUAL;
+
+
+-- 수료일까지 남은 기간 확인(날짜 기준) --> 단위 초
+--SELECT (수료일까지 남은 일수) * (24*60*60)
+--FROM DUAL;
+
+-- 수료일까지 남은 전체 초
+SELECT (TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)
+FROM DUAL;
+--==>> 10980823.99999999999999999999999999999997
+
+
+-- 『수료일까지 남은 전체 초』를 ... 다시.『일, 시간, 분, 초』로 환산하면...
+SELECT TRUNC(TRUNC(TRUNC(93784/60)/60)/24) "일"
+     , MOD(TRUNC(TRUNC(93784/60)/60), 24)"시간"
+     , MOD(TRUNC(93784/60), 60) "분"
+     , MOD(93784, 60) "초"
+FROM DUAL;
+
+SELECT TRUNC(TRUNC(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60)/60)/24) "일"
+     , MOD(TRUNC(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60)/60), 24)"시간"
+     , MOD(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60), 60) "분"
+     , TRUNC(MOD((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60), 60)) "초"
+FROM DUAL;
+
+SELECT SYSDATE "현재시각"                                                                                                 -- 2025-07-01 15:10:32
+     , TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') "수료일"     
+     , TRUNC(TRUNC(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60)/60)/24) "일"
+     , MOD(TRUNC(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60)/60), 24)"시간"
+     , MOD(TRUNC((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60)/60), 60) "분"
+     , TRUNC(MOD((TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) * (24*60*60), 60)) "초"
+FROM DUAL;
+/*
+2025-07-01 15:59:56    2025-11-05 18:00:00    127    2    0    4
+*/
+
+-- 테스트용 쿼리문
+SELECT SYSDATE "현재시각"                                                                                                 -- 2025-07-01 15:10:32
+    , TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') "수료일"                                                    -- 2025-11-05 18:00:00
+    , TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE "일_소수포함"                                     -- 127.117685185185185185185185185185185185
+    , TRUNC(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) "일"                                      -- 127
+    , (MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24) "시간_소수포함"                      -- 2.82444444444444444444444444444444444
+    , TRUNC(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24) "시간"                         -- 2
+    , TRUNC(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24) "시간"                         -- 2
+    , (MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60) "분_소수포함"             -- 49.4666666666666666666666666666666664
+    , TRUNC(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60) "분"                -- 49
+    , (MOD(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60, 1)*60) "초_소수포함"  -- 27.999999999999999999999999999999984
+    , TRUNC(MOD(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60, 1)*60) "초"      -- 27
+FROM DUAL;
+
+-- 테스트용 쿼리문
+SELECT SYSDATE "현재시각"
+    , TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') "수료일"
+    , TRUNC(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) "일"
+    , TRUNC(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24) "시간"
+    , TRUNC(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60) "분"
+    , TRUNC(MOD(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1)*24, 1)*60, 1)*60) "초"
+FROM DUAL;
+
+
+-- COPILOT 자동완성....
+SELECT SYSDATE "현재시각"
+     , TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') "수료일"
+     , TRUNC(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE) "일"
+     , TRUNC(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1) * 24) "시간"
+     , TRUNC(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1) * 24, 1) * 60) "분"
+     , TRUNC(MOD(MOD(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 1) * 24, 1) * 60, 1) * 60) "초"
+FROM DUAL;
+
+
+SELECT  TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE
+     , TRUNC(MOD(TO_DATE('2025-11-05 18:00:00', 'YYYY-MM-DD HH24:MI:SS') - SYSDATE, 24))
+FROM DUAL;
+
+
+
+--▣ 현재 날짜 및 시간을 기준으로...
+--   본인이 태어나서 현재까지...
+--   얼마만큼의 일, 시간, 분, 초를 살고있는지
+--   다음과 같은 형태로 조회할 수 있도록 쿼리문을 구성한다.
+/*
+-----------------------------------------------------------------------------
+현재시각             |  태어난시각          | 일   | 시간    | 분   | 초
+-----------------------------------------------------------------------------
+2025-07-01 14:31:27 | 1999-11-05 12:00:00 | XXXX | XX      | XX  | XX
+-----------------------------------------------------------------------------
+*/
+-- 과제 제출 공유폴더 > 20250701~~오라클 쿼리문> 홍길동.SQL 
+
+SELECT SYSDATE "현재시각"
+     , TO_DATE('1976-06-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS') "태어난시각"
+     , TRUNC(SYSDATE - TO_DATE('1976-06-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS')) "일"
+     , TRUNC(MOD(SYSDATE - TO_DATE('1976-06-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1) * 24) "시간"
+     , TRUNC(MOD(MOD(SYSDATE - TO_DATE('1976-06-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1) * 24, 1) * 60) "분"
+     , TRUNC(MOD(MOD(MOD(SYSDATE - TO_DATE('1976-06-01 12:00:00', 'YYYY-MM-DD HH24:MI:SS'), 1) * 24, 1) * 60, 1) * 60) "초"
+FROM DUAL;
+/*
+현재시각                      태어난시각                 일          시간     분   초
+-----------------------     ---------------------    ----------   -------  ----- -------
+2025-07-01 16:14:10         1976-06-01 12:00:00         17927         4     14      9
+*/
+
+
+--▣ 날짜 관련 세션 설정 변경
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+--==> Session이(가) 변경되었습니다.
+
+
+-- ※ 날짜 데이터를 대상으로 반올림, 절삭을 수행할 수 있다.
+
+--▣ 날짜 반올림
+SELECT SYSDATE "현재시각"                       -- 2025-07-01   → 기본 현재 날짜
+     , ROUND(SYSDATE, 'YEAR') "년 단위 반올림"  -- 2026-01-01   →  년도까지 유효한 데이터(상반기, 하반기 기준)
+     , ROUND(SYSDATE, 'MONTH') "월 단위 반올림"  -- 2025-07-01  →  월까지 유효한 데이터(15일 기준)
+     , ROUND(SYSDATE, 'DD') "일 단위 반올림"     -- 2025-07-02  →  날짜까지 유효한 데이터(정오 기준)
+     , ROUND(SYSDATE, 'DAY') "일 단위 반올림"     -- 2025-06-29 →  날짜까지 유효한 데이터(수요일 기준)
+--     , ROUND(SYSDATE, 'HH') "시간 단위 반올림"
+--     , ROUND(SYSDATE, 'MI') "분 단위 반올림"
+--     , ROUND(SYSDATE, 'SS') "초 단위 반올림"
+FROM DUAL;
+/*
+2025-07-01    2026-01-01    2025-07-01    2025-07-02    2025-06-29
+*/
+
+
+
+--▣ 날짜 절삭
+SELECT SYSDATE "현재시각"                         -- 2025-07-01   → 기본 현재 날짜
+     , TRUNC(SYSDATE, 'YEAR') "년 단위 반올림"    -- 2025-01-01   →  년도까지 유효한 데이터
+     , TRUNC(SYSDATE, 'MONTH') "월 단위 반올림"   -- 2025-07-01   →  월까지 유효한 데이터
+     , TRUNC(SYSDATE, 'DD') "일 단위 반올림"      -- 2025-07-01   →  날짜까지 유효한 데이터
+     , TRUNC(SYSDATE, 'DAY') "일 단위 반올림"     -- 2025-06-29   →  날짜까지 유효한 데이터 → 지난주 일요일
+--     , ROUND(SYSDATE, 'HH') "시간 단위 반올림"
+--     , ROUND(SYSDATE, 'MI') "분 단위 반올림"
+--     , ROUND(SYSDATE, 'SS') "초 단위 반올림"
+FROM DUAL;
+
+----------------------------------------------------------------------------
+--▣▣▣  변환 함수   ▣▣▣--
+-- TO_CHAR()  : 날짜 → 문자(숫자나 날짜 데이터를 문자 타입으로 변환시켜주는 함수)
+-- TO_DATE()  : 문자 → 날짜(문자 데이터(날짜 형식)를 날짜 타입으로 변환시켜주는 함수)
+-- TO_NUMBER(): 문자 → 숫자(문자 데이터(숫자 형식)를 숫자 타입으로 변환시켜주는 함수)
+
+SELECT 10 "COL1"
+     , TO_CHAR(10) "COL2"          -- 문자로 변환
+     , TO_DATE('2025-07-01', 'YYYY-MM-DD') "COL3" -- 문자 → 날짜
+     , TO_NUMBER('1234') "COL4"    -- 문자 → 숫자
+     , TO_NUMBER('1234.56') "COL5" -- 문자 → 숫자
+FROM DUAL;
+--==>> 10    10    2025-07-01    1234    1234.56
+
+
+-- ※ 날짜나 통화 형식이 맞지 않을 경우
+--    세션 설정값을 통해 설정할 수 있도록 한다.
+ALTER SESSION SET NLS_DATE_LANGUAGE = 'KOREAN';
+--==>> Session이(가) 변경되었습니다.
+ALTER SESSION SET NLS_LANGUAGE = 'KOREAN';
+--==>> Session이(가) 변경되었습니다.
+ALTER SESSION SET NLS_NUMERIC_CHARACTERS = ',.';
+ALTER SESSION SET NLS_CURRENCY = '\'; -- 화폐단위 : 원(￦)
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+--==>> Session이(가) 변경되었습니다.
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS';
+--==>> Session이(가) 변경되었습니다.
+
+SELECT TO_CHAR(SYSDATE) "현재시각"
+FROM DUAL;
+--==>> 2025-07-01
+
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS') "현재시각"
+     , TO_CHAR(SYSDATE, 'YYYY-MM-DD') "날짜"
+     , TO_CHAR(SYSDATE, 'HH24:MI:SS') "시간"
+     , TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS', 'NLS_DATE_LANGUAGE = KOREAN') "한글 날짜"
+     , TO_CHAR(SYSDATE, 'YYYY-MM-DD HH24:MI:SS', 'NLS_DATE_LANGUAGE = ENGLISH') "영어 날짜"
+FROM DUAL;
+/*
+2025-07-01 16:51:55    2025-07-01    16:51:55    2025-07-01 16:51:55    2025-07-01 16:51:55
+*/
+
+SELECT TO_CHAR(SYSDATE, 'YYYY-MM-DD') -- 2025-07-01
+     , TO_CHAR(SYSDATE, 'YYYY')       -- 2025
+     , TO_CHAR(SYSDATE, 'YEAR')       -- TWENTY TWENTY-FIVE
+     , TO_CHAR(SYSDATE, 'MM')         -- 07
+     , TO_CHAR(SYSDATE, 'MONTH')      -- 7월 
+     , TO_CHAR(SYSDATE, 'MON')        -- 7월 
+     , TO_CHAR(SYSDATE, 'DD')         -- 01
+     , TO_CHAR(SYSDATE, 'DAY')        -- 화요일
+     , TO_CHAR(SYSDATE, 'DY')         -- 화
+     , TO_CHAR(SYSDATE, 'HH24')       -- 17
+     , TO_CHAR(SYSDATE, 'HH')         -- 05
+     , TO_CHAR(SYSDATE, 'HH AM')      -- 05 오후
+     , TO_CHAR(SYSDATE, 'HH PM')      -- 05 오후
+     , TO_CHAR(SYSDATE, 'MI')         -- 54
+     , TO_CHAR(SYSDATE, 'SS')         -- 15
+     , TO_CHAR(SYSDATE, 'SSSSS')      -- 64455 , 금일 흘러온 초
+     , TO_CHAR(SYSDATE, 'Q')          -- 3, 분기
+FROM DUAL;
+
+
+
+--▣ EXTRACT() 함수
+-- EXTRACT() 함수는 날짜나 숫자에서 특정한 부분을 추출하는 함수이다.
+SELECT TO_CHAR(SYSDATE, 'YYYY') 
+     , TO_CHAR(SYSDATE, 'MM')
+     , TO_CHAR(SYSDATE, 'DD')
+     , EXTRACT(YEAR FROM SYSDATE) "연도"
+     , EXTRACT(MONTH FROM SYSDATE) "월"
+     , EXTRACT(DAY FROM SYSDATE) "일"
+     , EXTRACT(HOUR FROM SYSDATE) "시"
+     , EXTRACT(MINUTE FROM SYSDATE) "분"
+     , EXTRACT(SECOND FROM SYSDATE) "초"
+FROM DUAL;
+
+--▣ TO_CHAR() 함수 활용 추가 →  형식 맟춤 표기 결과값 반환
+SELECT 80000 "COL1"                           -- 80000
+     , TO_CHAR(80000)                  "COL2" -- 80000
+     , TO_CHAR(80000, '99,999')        "COL3" -- 80,000
+     , TO_CHAR(80000, '$99,999')       "COL4" -- $80,000
+     , TO_CHAR(80000, 'L99,999')       "COL5" -- ￦80,000
+     , TO_CHAR(80000, '9,999,999')     "COL6" -- 80,000
+     , TO_CHAR(80000, '9,999,999.99')  "COL7" -- 80,000.00
+     , TO_CHAR(80000, 'L9,999,999')    "COL8" -- ￦80,000
+     , TO_CHAR(80000, 'L9,999,999.99') "COL9" -- ￦80,000.00
+     , LTRIM(TO_CHAR(80000, 'L9,999,999.99'))  "COL10" -- ￦80,000.00
+FROM DUAL;
+--==>> 80000    80000     80,000     $80,000            ￦80,000        80,000        80,000.00               ￦80,000               ￦80,000.00    ￦80,000.00
+
+
+-- ※ 날짜 관련 세션 설정 변경
+ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD HH24:MI:SS';
+--==>> Session이(가) 변경되었습니다.
+
+--▣ 현재 시간을 기준으로 1일 2시간 3분 4초 후를 조회한다.
+SELECT SYSDATE "현재 시간"
+     , SYSDATE + 1 + 2/24 + 3/(24*60) + 4/(24*60*60) "연산 후 시간"
+FROM DUAL;
+/*
+2025-07-01 17:30:13
+2025-07-02 19:33:17
+*/
+
+--▣  현재 시간을 기준으로 1년 2개월 3일 4시간 5분 6초 후를 조회한다.
+SELECT SYSDATE "현재 시간"
+      , SYSDATE + TO_YMINTERVAL('01-02') + TO_DSINTERVAL('003 04:05:06') "연산 후 시간"
+FROM DUAL;
+/*
+2025-07-01 17:37:13    
+2026-09-04 21:42:19
+*/
+
+
+
+--▣▣▣ CASE 구문(조건문, 분기문)  ▣▣▣---
+/*
+CASE
+WHEN
+THEN
+ELSE
+END
+*/
+
+SELECT CASE 5+2 WHEN 7 THEN '5+2=7' ELSE '5+2는 몰라요'END "결과확인"
+FROM DUAL;
+--==>> 5+2=7
+
+SELECT CASE 5+3 WHEN 4 THEN '5+3=4' ELSE '5+3은 몰라요'END "결과확인"
+FROM DUAL;
+--==>> 5+3은 몰라요
+
+
+SELECT CASE 1+1 WHEN 2 THEN '1+1=2' 
+                WHEN 3 THEN '1+1=3'
+                WHEN 4 THEN '1+1=4'
+                ELSE '몰라요'
+       END "결과확인"
+FROM DUAL;
+--==>> 1+1=2
+
+
+
+--▣ DECODE() 함수
+-- DECODE() 함수는 특정한 값을 기준으로 조건을 검사하고, 그에 따라
+-- 대응하는 값을 반환하는 함수이다.
+SELECT DECODE(5+2, 7, '5+2=7', '5+2는 몰라요') "결과확인"
+FROM DUAL;
+--==>> 5+2=7
+
+SELECT DECODE(5-2, 1, '5-2=1', 2, '5-2=2', 3,  '5-2=3', '5-2는 몰라요') "결과확인"
+FROM DUAL;
+--==>> 5-2=3
+
+
+SELECT CASE WHEN 1+1=2 THEN '1+1=2' 
+            WHEN 1+2=5 THEN '1+2=5'
+            WHEN 1+3=7 THEN '1+3=7'
+            ELSE '몰라요'
+       END "결과확인"
+FROM DUAL;
+--==>> 1+1=2
 
 
 -- ### --▣ ※ ○ ★ 『』 ? ▣ ◀▶ ▼ ⓐ ⓑ ① ② ③ ④ ⑤ ⑥ ⑦ ⑧ ⑨ ⑩  →  ←  ↓  …  ： º↑ /* */  ─ ┃ ┛┯ ┐┘ ￦
