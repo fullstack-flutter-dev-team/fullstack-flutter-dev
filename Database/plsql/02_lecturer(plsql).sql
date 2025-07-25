380923a4a3e7ab802308010a780ce2b27001a509
quantum kim <quantumearth76@gmail.com>
2025-07-25 18:15:28 +0900

    로그인 프로시저 추가

---
+++ Database/plsql/02_lecturer(plsql).sql
@@ -0,0 +1,57 @@
+SELECT USER
+FROM DUAL;
+
+-- ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+-- ==>> Session이(가) 변경되었습니다.
+
+SET SERVEROUTPUT ON;
+-----------------------------
+CREATE OR REPLACE PROCEDURE proc_lecturer_login
+( P_ID IN VARCHAR2
+, P_PW IN VARCHAR2
+, R_MSG OUT VARCHAR2
+)
+IS
+    STATUS VARCHAR2(100):= 2;
+BEGIN
+    -- 기본 ID/PW : LECTURE_ID/SSN 끝 7자
+    -- NEW ID/PW : LOGIN_ID/LOGIN_PW
+    SELECT NVL2((SELECT  TO_CHAR(LECTURER_ID) FROM TBL_LECTURER WHERE  TO_CHAR(LECTURER_ID)=P_ID OR LOGIN_ID = P_ID), '0', '-1')  INTO STATUS
+      FROM DUAL;     
+    IF STATUS = -1 THEN
+        -- R_MSG := '아이디가 존재하지 않습니다.';
+        R_MSG := '-1';
+    ELSE -- 아이디가 조회된경우
+        SELECT NVL2((SELECT TO_CHAR(LECTURER_ID) FROM TBL_LECTURER
+                           WHERE (TO_CHAR(LECTURER_ID) = P_ID AND SUBSTR(SSN, 8) = P_PW )
+                            OR (LOGIN_ID = P_ID AND LOGIN_PW = P_PW)), '0', '-2') INTO R_MSG
+           FROM DUAL;
+    END IF;  
+END;
+
+----------------------------------------------
+-- 실행
+variable r_msg varchar2(300);
+exec proc_lecturer_login('BuleSKY','korea@',:r_msg);
+PRINT r_msg;
+
+----------------------------------------------
+SELECT *
+FROM TBL_LECTURER
+WHERE LECTURER_ID = 10000002;
+
+---------------------
+UPDATE TBL_LECTURER
+SET LOGIN_ID = 'BuleSKY', LOGIN_PW = 'korea@'
+WHERE LECTURER_ID = 10000002;
+
+----------------------------------------------
+-- 커밋
+COMMIT;
+ 
+
+------------------------------------------------------------------------------
+-- 커밋
+COMMIT;
+
+--------------------------------------------------------------------------------------------
---
+++ Database/plsql/03_student(plsql).sql
@@ -0,0 +1,55 @@
+SELECT USER
+FROM DUAL;
+
+-- ALTER SESSION SET NLS_DATE_FORMAT = 'YYYY-MM-DD';
+-- ==>> Session이(가) 변경되었습니다.
+
+SET SERVEROUTPUT ON;
+-----------------------------
+CREATE OR REPLACE PROCEDURE proc_student_login
+( P_ID IN VARCHAR2
+, P_PW IN VARCHAR2
+, R_MSG OUT VARCHAR2
+)
+IS
+    STATUS VARCHAR2(100):= 2;
+BEGIN
+    -- 기본 ID/PW : LECTURE_ID/SSN 끝 7자
+    -- NEW ID/PW : LOGIN_ID/LOGIN_PW
+    SELECT NVL2((SELECT  TO_CHAR(STUDENT_ID) FROM TBL_STUDENT WHERE  TO_CHAR(STUDENT_ID)=P_ID OR LOGIN_ID = P_ID), '0', '-1')  INTO STATUS
+      FROM DUAL;     
+    IF STATUS = -1 THEN
+        -- R_MSG := '아이디가 존재하지 않습니다.';
+        R_MSG := '-1';
+    ELSE -- 아이디가 조회된경우
+        SELECT NVL2((SELECT TO_CHAR(STUDENT_ID) FROM TBL_STUDENT
+                           WHERE (TO_CHAR(STUDENT_ID) = P_ID AND SUBSTR(SSN, 8) = P_PW )
+                            OR (LOGIN_ID = P_ID AND LOGIN_PW = P_PW)), '0', '-2') INTO R_MSG
+           FROM DUAL;
+    END IF;  
+END;
+
+----------------------------------------------
+-- 오라클 AMPERSAND(&)등의 특수 문자 insert
+SET DEFINE OFF;
+SET DEFINE %;
+
+-- 실행
+variable r_msg varchar2(300);
+exec proc_student_login('Miracle','@#world',:r_msg);
+PRINT r_msg;
+
+----------------------------------------------
+SELECT *
+FROM TBL_STUDENT
+WHERE STUDENT_ID = 20000004;
+
+---------------------
+UPDATE TBL_STUDENT
+SET LOGIN_ID = 'Miracle', LOGIN_PW = '@#world'
+WHERE STUDENT_ID = 20000004;
+
+----------------------------------------------
+-- 커밋
+COMMIT;
+--------------------------------------------------------------------------------------------
