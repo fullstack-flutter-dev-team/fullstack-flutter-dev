a4543717b26ed74464b5c96d8119cb93756c33cb
quantum kim <quantumearth76@gmail.com>
2025-10-20 06:54:13 +0900

    10월 20일 아침-2

---
+++ SpringMVCStudy/mvcApp04/src/com/test/mvc/PosListController.java
@@ -0,0 +1,52 @@
+package com.test.mvc;
+/**
+ * ========================================
+ *   PosListController.java
+ *    - 사용자 정의 컨트롤러 클래스
+ * ========================================
+ */
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// ※ Spring MVC 의 『Controller』 인터페이스를 구현하는 방법을 통해
+//    사용자 정의 컨트롤러 클래스를 구성할 수 있도록 한다.
+
+public class PosListController implements Controller
+{
+    private IPositionDAO positionDAO;
+    
+    public void setPositionDAO(IPositionDAO positionDAO)
+    {
+        this.positionDAO = positionDAO;
+    }
+ 
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        // Controller 가 수행해야 할 액션 코드
+        ModelAndView mav = new ModelAndView();
+        //-------------------------------------------------------
+        ModelAndView loginStatusMav= new LoginStatus(request, mav).check();
+        if (loginStatusMav.getViewName() != null) {
+            return loginStatusMav;
+        }//------------[로그인 상태 & 관리자 로그인 상태 체크]
+
+        String viewName = "/WEB-INF/view/PosList.jsp";
+        
+        try {
+            mav.addObject("positionList", positionDAO.list());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        mav.setViewName(viewName);
+        //-------------------------------------------------------
+        return mav;
+    }
+
+}
---
+++ SpringMVCStudy/mvcApp04/src/com/test/mvc/RegListController.java
@@ -0,0 +1,55 @@
+package com.test.mvc;
+/**
+ * ========================================
+ *   RegListController.java
+ *    - 사용자 정의 컨트롤러 클래스
+ *    - 지역 리스트 출력 액션 컨트롤러
+ *    - regList.action
+ * ========================================
+ */
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+// ※ Spring MVC 의 『Controller』 인터페이스를 구현하는 방법을 통해
+//    사용자 정의 컨트롤러 클래스를 구성할 수 있도록 한다.
+
+public class RegListController implements Controller
+{
+    private IRegionDAO regionDAO;
+
+    public void setRegionDAO(IRegionDAO regionDAO)
+    {
+        this.regionDAO = regionDAO;
+    }
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception
+    {
+        // Controller 가 수행해야 할 액션 코드
+        ModelAndView mav = new ModelAndView();
+        //-------------------------------------------------------
+        // [로그인 상태 & 관리자 로그인 상태 체크]---------------
+        ModelAndView loginStatusMav= new LoginStatus(request, mav).check();
+        if (loginStatusMav.getViewName() != null) {
+        	return loginStatusMav;
+        }//------------[로그인 상태 & 관리자 로그인 상태 체크]
+        
+        String viewName = "/WEB-INF/view/RegList.jsp";
+        
+        try {
+            mav.addObject("regionList", regionDAO.list());
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+        
+        mav.setViewName(viewName);
+        //-------------------------------------------------------
+        return mav;
+    }
+
+}
